37e856e017e88b685e04d241f410b70b6221dd7c
Jasper Vriends <77139@mjj.lan>
2016-06-16 10:49:03 +0200

    Jasper moving to other mac

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public int kills;
+    public int deaths;
+    public int Health;
+    public int score;
+    public int teamScore;
+
+    public GameObject ScoreText;
+    public GameObject DeathText;
+    public GameObject KillText;
+    public GameObject TeamScoreText;
+    public GameObject HealthText;
+
+    void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        addScore(34);
+
+    }
+
+
+
+    public void addDeath()
+    {
+        deaths++;
+        ScoreText.GetComponent<Text>().text = "Deaths: " + deaths;
+    }
+
+    public void addKill()
+    {
+        kills++;
+        KillText.GetComponent<Text>().text = "Kills: " + kills;
+    }
+
+    public void addScore(int amount)
+    {
+        score++;
+        ScoreText.GetComponent<Text>().text = "Score: " + score;
+    }
+
+    public void addTeamScore(int amount)
+    {
+        teamScore++;
+        TeamScoreText.GetComponent<Text>().text = "Team Score: " + teamScore;
+    }
+
+    public void damageHealth(int amount)
+    {
+        //
+    }
+
+}
