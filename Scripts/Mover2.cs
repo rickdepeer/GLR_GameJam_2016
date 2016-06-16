f1e39c2ee1e138f7b84a9942120e9d0ebb342b65
daanpothuis <daanpothuiz@gmail.com>
2016-06-16 03:23:12 +0200

    town scene commit

---
+++ Scripts/Mover2.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover2 : MonoBehaviour
+{
+    private Vector3 startPositie;
+    private Vector3 playerPositie;
+    private int waitToStart;
+
+    private int start = 0;
+
+    public Transform Player;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPositie = gameObject.transform.position;
+        playerPositie = Player.gameObject.transform.position;
+        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * -1000f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        print(gameObject.transform.position);
+        print(startPositie);
+
+        if (gameObject.transform.position.z > 70)
+        {
+            print("TEST");
+            gameObject.transform.position = startPositie;
+        }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Player.transform.position = playerPositie;
+        }
+    }
+}
